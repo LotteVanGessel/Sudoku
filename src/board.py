d1cd2e270b4ba2b5a159d8809266429180af9d7c
Lotte Gessel, van <l.s.v.gessel@student.tue.nl>
2024-08-07 11:44:11 +0200

    added containers

--- src/board.py
+++ src/board.py
@@ -8,14 +8,19 @@ class Board:
 
     def __init__(self) -> None:
         self.squares = []
-        self._create()
         self.rows = [Row() for _ in range(ROWS)]
         self.columns = [Column() for _ in range(COLS)]
-        self.blocks = [Block() for _ in range(ROWS)]
+        self.blocks = [[Block() for _ in range(3)] for _ in range(3)]
+        self._create()
+        
 
     def _create(self):
         for row in range(ROWS):
             self.squares.append([])
             for col in range(COLS):
-                self.squares[row].append(Square(row, col)) 
+                new_square = Square(row, col)
+                self.squares[row].append(new_square)
+                self.rows[row].add_square(new_square) 
+                self.columns[col].add_square(new_square) 
+                self.blocks[row // 3][col // 3].add_square(new_square) 
                 
--- src/container.py
+++ src/container.py
@@ -1,15 +1,23 @@
 class Container:
     def __init__(self) -> None:
         self.contained_squares = []
+    
+    def add_square(self, square):
+        self.contained_squares.append(square)
+
+    def remove_possible_number(self, number):
+        for square in self.contained_squares:
+            if number in square.possible_numbers:
+                square.possible_numbers.remove(number)
 
 class Row(Container):
     def __init__(self) -> None:
-        super.__init__()
+        super().__init__()
 
 class Block(Container):
     def __init__(self) -> None:
-        super.__init__()
+        super().__init__()
 
 class Column(Container):
     def __init__(self) -> None:
-        super.__init__()
+        super().__init__()
